46573c7bce6d0d3367e10b4b31e8e0044fb21886
beyza.gumus <beyza.gumus>
2023-12-14 11:50:00 +0300

    explain @SpringBootApplication details

--- src/main/java/com/ebg/Main.java
+++ src/main/java/com/ebg/Main.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@SpringBootApplication is used instead of @ComponentScan , @EnableAutoConfiguration
+//@SpringBootApplication is used instead of @ComponentScan , @EnableAutoConfiguration, @Configuration
 //@SpringBootApplication
 @ComponentScan(basePackages = "com.ebg")
 //I'm telling it the package it should look for components
 @EnableAutoConfiguration
+@Configuration
+//If we have any bean inside that we want to instantiate, this works
 @RestController
 public class Main {
     public static void main(String[] args) {
