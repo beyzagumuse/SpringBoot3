127f5aa909a7ebb886f083ff735ea0d8fc95471f
beyza.gumus <beyza.gumus>
2023-12-14 11:25:17 +0300

    @GetMapping("...") at ... part you decide the link after localhost:8080, now I see Hello at localhost:8080/hi

--- src/main/java/com/ebg/Main.java
+++ src/main/java/com/ebg/Main.java
@@ -2,11 +2,19 @@ package com.ebg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/hi")
+    public String greet(){
+        return "Hello";
     }
 
 }
