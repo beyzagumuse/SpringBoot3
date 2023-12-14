fbfcfed1ff477309f35480864bf4c7dba7ffdf23
beyza.gumus <beyza.gumus>
2023-12-14 12:05:01 +0300

    record class

--- src/main/java/com/ebg/Main.java
+++ src/main/java/com/ebg/Main.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -15,15 +17,47 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    //Rest endpoint to create APIs
     @GetMapping("/greet")
     public GreetResponse greet(){
         return new GreetResponse("Hello");
     }
 
-    //We now have JSON as return up there
-    record GreetResponse(String greet){
+    //records are classes that allows us to achieve immutability
+    //record GreetResponse(String greet){
 
+
+    //record creates right below class and codes in it
+    class GreetResponse {
+        private final String greet;
+
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
     }
 
 }
