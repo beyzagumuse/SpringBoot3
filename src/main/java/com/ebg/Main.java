c407a0f059d11b5fe3315a846436e7a8e75ffe9a
beyza.gumus <beyza.gumus>
2023-12-14 11:47:50 +0300

    explain @SpringBootApplication details and @ComponentScan package use

--- src/main/java/com/ebg/Main.java
+++ src/main/java/com/ebg/Main.java
@@ -1,18 +1,24 @@
 package com.ebg;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication is used instead of @ComponentScan , @EnableAutoConfiguration
+//@SpringBootApplication
+@ComponentScan(basePackages = "com.ebg")
+//I'm telling it the package it should look for components
+@EnableAutoConfiguration
 @RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/hi")
+    @GetMapping("/greet")
     public String greet(){
         return "Hello";
     }
