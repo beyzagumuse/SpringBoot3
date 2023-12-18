c649b24a82c02ad913117ccf7b7fb764c8e52048
beyza.gumus <beyza.gumus>
2023-12-18 11:58:54 +0300

    create db

--- src/main/java/com/ebg/Main.java
+++ src/main/java/com/ebg/Main.java
@@ -32,38 +32,4 @@ public class Main {
     record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person){}
 
 
-    //record creates right below class and codes in it
-//    class GreetResponse {
-//        private final String greet;
-//
-//
-//        GreetResponse(String greet) {
-//            this.greet = greet;
-//        }
-//
-//        public String getGreet() {
-//            return greet;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' +
-//                    '}';
-//        }
-//
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            GreetResponse that = (GreetResponse) o;
-//            return Objects.equals(greet, that.greet);
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return Objects.hash(greet);
-//        }
-//    }
-
 }
